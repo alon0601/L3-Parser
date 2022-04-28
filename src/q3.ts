ff5f20a3f946349eaf2a413daa4fba98fe58c350
yuval <yamyuv@post.bgu.ac.il>
2022-04-28 09:42:24 +0300

    q3 done!

--- src/q3.ts
+++ src/q3.ts
@@ -1,5 +1,5 @@
-import {Exp,LetExp,AppExp,VarDecl,LetStarExp, Program, isProgram, isExp, makeProgram, isCExp, makeDefineExp, isDefineExp, isAtomicExp, isIfExp, isLitExp, CExp, makeIfExp, isAppExp, makeAppExp, makeProcExp, isLetExp, isProcExp} from "./L31-ast";
-import { Result, makeFailure } from "../shared/result";
+import {Exp,LetExp,AppExp,VarDecl,LetStarExp,isLetStarExp, Program, isProgram, isExp, makeProgram, isCExp, makeDefineExp, isDefineExp, isAtomicExp, isIfExp, isLitExp, CExp, makeIfExp, isAppExp, makeAppExp, makeProcExp, isLetExp, isProcExp, makeLetExp, makeLetStarExp} from "./L31-ast";
+import { Result, makeFailure,makeOk } from "../shared/result";
 import { map, pipe, zipWith } from "ramda";
 
 
@@ -14,40 +14,38 @@ Purpose: rewrite a single LetExp as a lambda-application form
 Signature: rewriteLet(cexp)
 Type: [LetExp => AppExp]
 */
-const rewriteLetStar = (e: LetStarExp): AppExp => {
-    const lets : LetExp[] = e.bindings;
-    const vals : CExp[] = map((b) => b.val, e.bindings);
-    return makeAppExp(
-            makeProcExp(vars, e.body),
-            vals);
-}
+const rewriteLetStar = (e: LetStarExp): LetExp =>
+    (e.bindings.length === 1) ? makeLetExp(e.bindings, e.body):
+        makeLetExp(e.bindings.slice(0,1),[makeLetStarExp(e.bindings.slice(1),e.body)])
+        
 
 /*
 Purpose: rewrite all occurrences of let in an expression to lambda-applications.
 Signature: rewriteAllLet(exp)
 Type: [Program | Exp -> Program | Exp]
 */
-export const rewriteAllLet = (exp: Program | Exp): Program | Exp =>
-    isExp(exp) ? rewriteAllLetExp(exp) :
-    isProgram(exp) ? makeProgram(map(rewriteAllLetExp, exp.exps)) :
+export const rewriteAllLetStar = (exp: Program | Exp): Program | Exp =>
+    isExp(exp) ? rewriteAllLetStarExp(exp) :
+    isProgram(exp) ? makeProgram(map(rewriteAllLetStarExp, exp.exps)) :
     exp;
 
-const rewriteAllLetExp = (exp: Exp): Exp =>
-    isCExp(exp) ? rewriteAllLetCExp(exp) :
-    isDefineExp(exp) ? makeDefineExp(exp.var, rewriteAllLetCExp(exp.val)) :
+const rewriteAllLetStarExp = (exp: Exp): Exp =>
+    isCExp(exp) ? rewriteAllLetStarCExp(exp) :
+    isDefineExp(exp) ? makeDefineExp(exp.var, rewriteAllLetStarCExp(exp.val)) :
     exp;
 
-const rewriteAllLetCExp = (exp: CExp): CExp =>
+const rewriteAllLetStarCExp = (exp: CExp): CExp =>
     isAtomicExp(exp) ? exp :
     isLitExp(exp) ? exp :
-    isIfExp(exp) ? makeIfExp(rewriteAllLetCExp(exp.test),
-                             rewriteAllLetCExp(exp.then),
-                             rewriteAllLetCExp(exp.alt)) :
-    isAppExp(exp) ? makeAppExp(rewriteAllLetCExp(exp.rator),
-                               map(rewriteAllLetCExp, exp.rands)) :
-    isProcExp(exp) ? makeProcExp(exp.args, map(rewriteAllLetCExp, exp.body)) :
-    isLetExp(exp) ? rewriteAllLetCExp(rewriteLet(exp)) :
+    isIfExp(exp) ? makeIfExp(rewriteAllLetStarCExp(exp.test),
+                            rewriteAllLetStarCExp(exp.then),
+                            rewriteAllLetStarCExp(exp.alt)) :
+    isAppExp(exp) ? makeAppExp(rewriteAllLetStarCExp(exp.rator),
+                               map(rewriteAllLetStarCExp, exp.rands)) :
+    isProcExp(exp) ? makeProcExp(exp.args, map(rewriteAllLetStarCExp, exp.body)) :
+    isLetStarExp(exp) ? rewriteAllLetStarCExp(rewriteLetStar(exp)) :
+    isLetExp(exp) ? makeLetExp(exp.bindings,map(rewriteAllLetStarCExp,exp.body)):
     exp;
 
 export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> =>
-    makeFailure("TODO");
+    makeOk(rewriteAllLetStar(exp))
--- src/q4.ts
+++ src/q4.ts
@@ -1,10 +1,47 @@
-import { Exp, Program } from '../imp/L3-ast';
-import { Result, makeFailure } from '../shared/result';
+import {Exp,LetExp,isPrimOp,isVarRef,LitExp,isStrExp,isNumExp,isBoolExp,,AppExp,VarDecl,Binding,ProcExp,LetStarExp,isLetStarExp, Program, isProgram, isExp, makeProgram, isCExp, makeDefineExp, isDefineExp, isAtomicExp, isIfExp, isLitExp, CExp, makeIfExp, isAppExp, makeAppExp, makeProcExp, isLetExp, isProcExp, makeLetExp, makeLetStarExp} from "./L31-ast";
+import { Result, makeFailure,makeOk } from "../shared/result";
+import { map, pipe, zipWith } from "ramda";
+import {valueToString,isEmptySExp,isSymbolSExp,isCompoundSExp} from "../imp/L3-value";
 
 /*
 Purpose: Transform L3 AST to JavaScript program string
 Signature: l30ToJS(l2AST)
 Type: [EXP | Program] => Result<string>
 */
+
+// const unparseLitExp = (le: LitExp): string =>
+//     isEmptySExp(le.val) ? `'()` :
+//     isSymbolSExp(le.val) ? `'${valueToString(le.val)}` :
+//     isCompoundSExp(le.val) ? `'${valueToString(le.val)}` :
+//     `${le.val}`;
+
+
+export const unparseL31 = (exp: Program | Exp): string =>
+    isBoolExp(exp) ? valueToString(exp.val) :
+    isNumExp(exp) ? valueToString(exp.val) :
+    isStrExp(exp) ? valueToString(exp.val) :
+    isLitExp(exp) ? unparseLitExp(exp) :
+    isVarRef(exp) ? exp.var :
+    isProcExp(exp) ? unparseProcExp(exp) :
+    isIfExp(exp) ? `(if ${unparseL31(exp.test)} ${unparseL31(exp.then)} ${unparseL31(exp.alt)})` :
+    isAppExp(exp) ? `(${unparseL31(exp.rator)} ${unparseLExps(exp.rands)})` :
+    isPrimOp(exp) ? exp.op :
+    isLetExp(exp) ? unparseLetExp(exp) :
+    isDefineExp(exp) ? `(define ${exp.var.var} ${unparseL31(exp.val)})` :
+    isProgram(exp) ? `(L31 ${unparseLExps(exp.exps)})` :
+    exp;
+
+const unparseLExps = (les: Exp[]): string =>
+    map(unparseL31, les).join(" ");
+
+const unparseProcExp = (pe: ProcExp): string => 
+    `(lambda (${map((p: VarDecl) => p.var, pe.args).join(",")}) ${unparseLExps(pe.body)})`
+
+
+const unparseLetExp = (le: LetExp) : string => 
+    `((${map((b: Binding) => b.var.var, le.bindings).join(",")}) => ${unparseLExps(le.body)}) (${map((b: Binding) => b.val, le.bindings).join(",")})`
+
 export const l30ToJS = (exp: Exp | Program): Result<string>  => 
     makeFailure("TODO");
+
+
